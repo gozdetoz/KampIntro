1962186918b850e892c684f17eb0284410593078
gozdetoz <gozdetoz62@gmail.com>
2021-01-28 21:22:17 +0300

    gameProject Eklendi

---
+++ GameProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GameProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GamerManager gamerManager = new GamerManager(new UserValidationManager());
+            gamerManager.Add
+                (new Gamer  
+                { Id = 1 , 
+                  BirthYear = 1993,
+                  FirstName = "GÖZDE",
+                  LastName = "TOZ",
+                  IdentityNumber = 12345678901 
+                }
+                );
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
